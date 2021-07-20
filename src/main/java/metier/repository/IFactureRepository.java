21597d6afafe9e78ebd4272768a5931b59158f46
rgarnaud <garnaud_raphael@outlook.com>
2021-07-20 13:01:11 +0200

    spring Jpa

--- src/main/java/metier/repository/IFactureRepository.java
+++ src/main/java/metier/repository/IFactureRepository.java
@@ -1,7 +1,9 @@
 package metier.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import metier.model.Facture;
 
-public interface IFactureRepository extends  IRepository<Facture,Long> {
+public interface IFactureRepository extends  JpaRepository<Facture,Long> {
 
 }
--- src/main/java/metier/repository/IPersonneRepository.java
+++ src/main/java/metier/repository/IPersonneRepository.java
@@ -1,7 +1,9 @@
 package metier.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import metier.model.Personne;
 
-public interface IPersonneRepository extends  IRepository<Personne,Long> {
+public interface IPersonneRepository extends  JpaRepository<Personne,Long> {
 
 }
